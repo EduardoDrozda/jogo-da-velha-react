2a3b55477952de5c60f331473acaa7c5ca6d7dd0
Eduardo Augusto Drozda dos Reis <eduardo.reis@elotech.com.br>
2018-10-11 16:51:49 -0300

    initial commit

---
+++ src/components/MoveTable.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+export default class MoveTable extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {}
+    }
+
+    render() {
+        return (
+            <p>Tabela</p>
+        )
+    }
+}
